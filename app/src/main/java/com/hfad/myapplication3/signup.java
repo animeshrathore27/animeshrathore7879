7d128afed5f97db6c6fb2b2aa941e376ee932dad
Animesh Rathore <rathoreanimesh3@gmail.com>
2022-04-02 20:29:22 +0530

    Initial commit

---
+++ app/src/main/java/com/hfad/myapplication3/signup.java
@@ -0,0 +1,39 @@
+package com.hfad.myapplication3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class signup extends AppCompatActivity {
+    EditText name, username, email, phone, password;
+    Button button;
+     FirebaseDatabase rootNode;
+     DatabaseReference reference;
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+        name= findViewById(R.id.name);
+        username=findViewById(R.id.username);
+        email=findViewById(R.id.email);
+        phone=findViewById(R.id.phone);
+        password=findViewById(R.id.password);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rootNode= FirebaseDatabase.getInstance();
+                reference=rootNode.getReference("registration");
+                reference.setValue("first data storage");
+            }
+        });
+    }
+}
